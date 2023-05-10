a5bc32f05d28af8251be06084f27c805d6fcc21c
Rene Stange <rsta2@o2online.de>
2023-05-10 22:25:45 +0200

    Update copyright year

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ import sphinx_rtd_theme
 # -- Project information -----------------------------------------------------
 
 project = 'Circle'
-copyright = '2020-2022, Rene Stange'
+copyright = '2020-2023, Rene Stange'
 author = 'Rene Stange'
 
 
